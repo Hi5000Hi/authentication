d19476613aa8500f6ebd2ab0d2089aab6dcd6fc1
Hi5000Hi <54914761+Hi5000Hi@users.noreply.github.com>
2019-09-04 15:27:41 -0600

    Update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyDKlIeYO0jIJpAGyqa1JIfl_sAIyw_RNrg",
+  authDomain: "authentication-eb85d.firebaseapp.com",
+  databaseURL: "https://authentication-eb85d.firebaseio.com",
+  projectId: "authentication-eb85d",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "602704770815",
+  appId: "1:602704770815:web:64a0ccc2e8687a6581beca"
 };
 
 firebase.initializeApp(config);
